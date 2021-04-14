e62defc7acaabb5b44e1d65b599b8a179311159a
mikesfakegithub <mikesfakegithub@gmail.com>
2021-04-14 16:05:10 -0700

    adds input for env variable controlling port number  On branch master  Changes to be committed: 	new file:   .git_ignore 	modified:   app.js

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require('express')
 const app = express()
 
+const port = process.env.PORT || 8080
+
 app.get('/', function(req, res){
 	res.send('=)')
 })
 
-app.listen(8080)
+app.listen(port, ()=>{
+    console.log(`now running on ${port}`)
+})
